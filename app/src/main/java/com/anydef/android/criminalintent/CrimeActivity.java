7d4a4ac27af073aa07ea618f253ef0c36e635ab9
ilbcj <ilbcj@hotmail.com>
2018-02-06 18:32:46 +0800

    7 unfinish 2

--- app/src/main/java/com/anydef/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/anydef/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,11 @@
 package com.anydef.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static com.anydef.android.criminalintent.R.id.fragment_container;
 
 public class CrimeActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(fragment_container);
+        if( fragment == null ) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(fragment_container).commit();
+        }
     }
 }
